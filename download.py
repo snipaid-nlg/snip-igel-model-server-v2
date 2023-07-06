a07390ebcfa77c1467f934443517f7560eefd2c0
Hannah Greven <greven.hannah@gmail.com>
2023-07-06 12:23:06 +0200

    ✨ Add preload for snip-igel model weights

--- download.py
+++ download.py
@@ -1,11 +1,26 @@
 # This file runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface BERT model
-from transformers import pipeline
+# Here: Our news snippet generation model snip-igel-500-v2
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    print("downloading model checkpoint...")
+    model = AutoModelForCausalLM.from_pretrained(
+        "malteos/bloom-6b4-clp-german",
+    )
+    print("done")
+    
+    # load LoRA adapters finetuned for news snippet generation
+    print("downloading adapter checkpoint")
+    PeftModel.from_pretrained(model, "snipaid/snip-igel-500-v2")
+    print("done")
+
+    # load tokenizer
+    print("loading tokenizer...")
+    AutoTokenizer.from_pretrained("philschmid/instruct-igel-001")
+    print("done")
 
 if __name__ == "__main__":
     download_model()
