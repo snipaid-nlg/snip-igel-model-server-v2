2974e6d1d8553bfb91ee971d459db7f9a293161e
Hannah Greven <greven.hannah@gmail.com>
2023-07-06 12:24:54 +0200

    ✨ Add initialization of snip-igel model

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from potassium import Potassium, Request, Response
 
-from transformers import pipeline
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+
 import torch
 
 app = Potassium("my_app")
@@ -8,11 +10,29 @@ app = Potassium("my_app")
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    # load model
+    print("loading base model...")
+    model = AutoModelForCausalLM.from_pretrained(
+        "malteos/bloom-6b4-clp-german",
+        torch_dtype=torch.float16,
+        load_in_8bit=True,
+        device_map='auto'
+    )
+    print("done")
+    
+    # load LoRA adapters finetuned for news snippet generation
+    print("downloading adapter checkpoint")
+    PeftModel.from_pretrained(model, "snipaid/snip-igel-500-v2")
+    print("done")
+
+    # load tokenizer
+    print("loading tokenizer...")
+    tokenizer = AutoTokenizer.from_pretrained("philschmid/instruct-igel-001")
+    print("done")
    
     context = {
-        "model": model
+        "model": model,
+        "tokenizer": tokenizer
     }
 
     return context
